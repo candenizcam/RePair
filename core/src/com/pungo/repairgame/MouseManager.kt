47062d8b1a8e7d5ecba7e079b59c01369778e706
e2036192 <e2036192@ceng.metu.edu.tr>
2020-02-01 01:57:24 +0300

    Code refactoring

--- core/src/com/pungo/repairgame/MouseManager.kt
+++ core/src/com/pungo/repairgame/MouseManager.kt
@@ -5,14 +5,5 @@ import com.badlogic.gdx.Input
 
 class MouseManager(var activeScreen: Screen){
 
-    fun checkMouse() {
-        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
-            Coordinates(Gdx.input.x, Gdx.input.y)
-    }
-
-    fun drawTentacle() {
-        // draw tentacle at (Gdx.input.x, Gdx.input.y)
-    }
-
 
 }
--- core/src/com/pungo/repairgame/SharedVariables.kt
+++ core/src/com/pungo/repairgame/SharedVariables.kt
@@ -6,4 +6,9 @@ object SharedVariables{
     val companyLogoRatio = 0.9 //1 is fullsize, 2 is double size 0.5 is half size... MATH...
     val companyLogoPath = "graphics/pungotitle.png"
     val mainMenuBackgroundPath = "graphics/main_menu_placeholder.png"
+
+    var loadingScreen = LoadingScreen()
+    var mainMenuScreen = MainMenuScreen()
+    var activeScreen : Screen = loadingScreen
+    var mouse = MouseManager(activeScreen)
 }
