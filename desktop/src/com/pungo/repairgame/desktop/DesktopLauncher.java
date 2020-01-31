38b4cc47fb9d8b97f9f2876dd82b3731111cdc50
chestarian <can.deniz.cam142@gmail.com>
2020-01-31 18:49:45 +0300

    Initial commit

---
+++ desktop/src/com/pungo/repairgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pungo.repairgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pungo.repairgame.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
