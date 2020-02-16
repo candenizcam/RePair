b96e98417ec77f693ec6a6804f9ef3e52686e87e
chestarian <can.deniz.cam142@gmail.com>
2020-02-16 21:19:26 +0300

    credits var

--- core/src/com/pungo/repairgame/optionsscreen/OptionsScreen.kt
+++ core/src/com/pungo/repairgame/optionsscreen/OptionsScreen.kt
@@ -1,21 +1,19 @@
-package com.pungo.repairgame.mainmenu
+package com.pungo.repairgame.optionsscreen
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.pungo.repairgame.*
-import java.util.*
 import java.util.Timer
 import kotlin.concurrent.schedule
 import kotlin.system.exitProcess
 
-class MainMenuScreen: Screen() {
+class OptionsScreen: Screen() {
     private lateinit var mainSprite: Sprite
-    private lateinit var titleSprite: Sprite
-    private lateinit var startButton: SetButton
-    private lateinit var continueButton: SetButton
-    private lateinit var optionsButton: SetButton
-    private lateinit var muteButton: ToggleButton
+    //private lateinit var startButton: SetButton
+    private lateinit var backButton: SetButton
+    //private lateinit var optionsButton: SetButton
+    //private lateinit var muteButton: ToggleButton
     private var sfx = Gdx.audio.newSound(Gdx.files.internal("sound/Blip.mp3"))
 
 
@@ -24,33 +22,37 @@ class MainMenuScreen: Screen() {
             setCenterX(SharedVariables.mainWidth.toFloat() / 2)
             setCenterY(SharedVariables.mainHeight.toFloat() / 2)
         }
-        titleSprite = SharedVariables.loadSprite(SharedVariables.titlePath, 0.5).apply{
-            setCenterX(SharedVariables.mainWidth.toFloat() / 2)
-            setCenterY(SharedVariables.mainHeight.toFloat() / 2)
+        backButton = SetButton("graphics/options_buttons/back", ratio = 1f).apply{
+            relocateCentre(SharedVariables.mainWidth*0.2f,SharedVariables.mainHeight*0.1f)
         }
-        startButton = SetButton("graphics/menu_buttons/start", ratio = 0.2f)
-        startButton.relocateCentre(SharedVariables.mainWidth.toFloat() / 2, 450f)
-        continueButton = SetButton("graphics/menu_buttons/continue", ratio = 0.2f)
-        continueButton.relocateCentre(SharedVariables.mainWidth.toFloat() / 2, 300f)
-        optionsButton = SetButton("graphics/menu_buttons/options", ratio = 0.2f).apply{
-            relocateCentre(SharedVariables.mainWidth.toFloat() / 2, 150f)
-        }
-        muteButton = ToggleButton("graphics/menu_buttons/mute",ratio=0.25f).apply{
-            relocateCentre(1820f, 980f)
-        }
+
+        //startButton = SetButton("graphics/menu_buttons/start", ratio = 0.2f)
+        //startButton.relocateCentre(SharedVariables.mainWidth.toFloat() / 2, 450f)
+        //continueButton = SetButton("graphics/menu_buttons/continue", ratio = 0.2f)
+        //continueButton.relocateCentre(SharedVariables.mainWidth.toFloat() / 2, 300f)
+        //optionsButton = SetButton("graphics/menu_buttons/options", ratio = 0.2f).apply{
+        //    relocateCentre(SharedVariables.mainWidth.toFloat() / 2, 150f)
+        //}
+        //muteButton = ToggleButton("graphics/menu_buttons/mute",ratio=0.25f).apply{
+        //    relocateCentre(1820f, 980f)
+        //}
     }
 
     override fun draw(batch: SpriteBatch) {
         mainSprite.draw(batch)
-        titleSprite.draw(batch)
-        startButton.draw(batch)
-        continueButton.draw(batch)
-        optionsButton.draw(batch)
-        muteButton.draw(batch)
+        backButton.draw(batch)
+        //startButton.draw(batch)
+        //continueButton.draw(batch)
+        //optionsButton.draw(batch)
+        //muteButton.draw(batch)
     }
 
     override fun firstPress() {
         when {
+            SharedVariables.contains(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), backButton.getBoundSprite()) -> {
+                backButton.status = ButtonStatus.DOWN
+            }
+            /*
             SharedVariables.contains(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), startButton.getBoundSprite()) -> {
                 startButton.status = ButtonStatus.DOWN
             }
@@ -71,15 +73,19 @@ class MainMenuScreen: Screen() {
                     }
 
                 }
-
-
             }
+
+             */
         }
     }
 
     override fun pressing() {
 
         when {
+            backButton.status == ButtonStatus.DOWN -> {
+                if (!SharedVariables.contains(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), backButton.activeSprite)) backButton.status = ButtonStatus.UP
+            }
+            /*
             startButton.status == ButtonStatus.DOWN -> {
                 if (!SharedVariables.contains(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), startButton.activeSprite)) startButton.status = ButtonStatus.UP
             }
@@ -89,11 +95,21 @@ class MainMenuScreen: Screen() {
             optionsButton.status == ButtonStatus.DOWN -> {
                 if (!SharedVariables.contains(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), optionsButton.activeSprite)) optionsButton.status = ButtonStatus.UP
             }
+
+             */
         }
     }
 
     override fun released() {
         when {
+            backButton.status == ButtonStatus.DOWN -> {
+                backButton.status = ButtonStatus.UP
+                sfx.play(SharedVariables.sfxVolume)
+                Timer().schedule(370) {
+                    SharedVariables.activeScreen = SharedVariables.mainMenuScreen
+                }
+            }
+            /*
             startButton.status == ButtonStatus.DOWN -> {
                 startButton.status = ButtonStatus.UP
                 sfx.play(SharedVariables.sfxVolume)
@@ -109,9 +125,10 @@ class MainMenuScreen: Screen() {
             }
             optionsButton.status == ButtonStatus.DOWN -> {
                 optionsButton.status = ButtonStatus.UP
-                SharedVariables.activeScreen = SharedVariables.optionsScreen
-                //SharedVariables.activeScreen = SharedVariables.creditsScreen
+                SharedVariables.activeScreen = SharedVariables.creditsScreen
             }
+
+             */
         }
     }
 
